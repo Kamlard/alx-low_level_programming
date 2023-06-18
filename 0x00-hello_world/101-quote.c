cefc320a45f3926fc4f8a787df62ec147db6c020
olayemi kamolideen lanre <klolayemi12@gmail.com>
2023-06-18 09:25:47 -0700

    hello c world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a c program that print a line to the standard error
- * Return: 1 (Success)
+ * main - and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Return: 1 (error)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
